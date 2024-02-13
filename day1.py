845abb5f8c8ea3655adf20eaa054129136228ec1
Beatriz Brito <beatriz.debritolima1998@gmail.com>
2024-02-13 17:03:36 -0300

    day1

---
+++ day1.py
@@ -0,0 +1,17 @@
+# exercies:
+
+# 1. Print your age to the console:
+print(25)
+
+# 2. Calculate and print the number of days, weeks, and months in 27 years. Don't worry
+# about leap years!
+days = 27 * 365
+weeks = 27 * 52
+months = 27 * 12
+print(f"in 27 years: {days} days, {weeks} weeks and {months} months.")
+
+# 3. Calculate and print the area of a circle with a radius of 5 units. You can be as
+# accurate as you like with the value of pi.
+pi = 3.14
+a = pi * (5**2)
+print(a)
