2da997cb874dd32f4efc62a05260f8c6282c4edf
Beatriz Brito <beatriz.debritolima1998@gmail.com>
2024-02-15 13:51:15 -0300

    day 3

---
+++ day3.py
@@ -0,0 +1,23 @@
+# Formatting Strings and Processing User Input
+
+# 1.
+greeting = "Hello, world"
+print(f"{greeting}" + "!")
+
+# 2.
+name = input("ask me your name: ")
+greeting = "Hello, "
+print(f"{greeting}" + f"{name}" + "!")
+
+# 3.
+age = 29
+age = str(age)
+print(f"I am {age}")
+
+# 4. 
+# string interpolation 
+title = "Joker"
+director = "Todd Phillips"
+release_year = 2019
+release_year = str(release_year)
+print(f"{title} "  + f"({release_year}) " + "directed by " + f"{director}")
