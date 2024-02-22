9baccea73eb9490d66acb3035e2849819d6badc7
Beatriz de Brito Lima <beatriz.lima@cvm.gov.br>
2024-02-22 14:40:14 -0300

    day 4 complete

--- day4.py
+++ day4.py
@@ -4,3 +4,30 @@
 # ex: tuple = (1, 2) or tuple = 1, 2
 
 # 1. 
+movies = [
+    ("the room", "tommy wiseau", "2003", "$6,000,000")
+]
+
+# 2. 
+title = input("Title: ")
+director_name = input("Director: ")
+release_year = input("Year of release: ")
+budget = input("Budget ")
+
+# 3.
+movie = title, director_name, release_year, budget
+
+# 4.
+print(f'{movie[0]} ({movie[2]})')
+
+# 5.
+movies.append(movie)
+
+# 6.
+print(movies[0])
+print(movies[1])
+
+# 7.
+movies.pop()
+#del movies[0]
+print(movies)
