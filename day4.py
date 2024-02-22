5e5144817f2d6eeb31144e06b0ba0b4fd73569fd
Beatriz de Brito Lima <beatriz.lima@cvm.gov.br>
2024-02-22 12:33:07 -0300

    day4

---
+++ day4.py
@@ -0,0 +1,6 @@
+# list: is a object can even mix different types and is a object changeable
+# ex: friend_details = ["John", 27, "Web Developer"]
+# tuple: one differences between tuples and list is that tuple are immutable
+# ex: tuple = (1, 2) or tuple = 1, 2
+
+# 1. 
